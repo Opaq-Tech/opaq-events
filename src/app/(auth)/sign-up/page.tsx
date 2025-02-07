bc3ad6944107186f7a5380ae69992c68ccebdd75
Teja2106 <teja182106@gmail.com>
2025-02-07 16:28:21 +0530

    made sign-up and sign-in page. Added validation for sign-up page only.

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import { z } from 'zod';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { signUpSchema } from '@/schemas/signUpSchema';
+import { zodResolver } from '@hookform/resolvers/zod';
+import Concert from '../../../../public/img/Concert.jpg';
+import DarkLogo from '../../../../public/img/opaqNOBG-01.png';
+import Image from 'next/image';
+
+type signUpData = z.infer<typeof signUpSchema>;
+
+export default function SignUp() {
+    const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm<signUpData>({ resolver: zodResolver(signUpSchema) });
+
+    const onSubmit: SubmitHandler<signUpData> = async(data) => {
+        console.log(data);
+    }
+    return (
+        <div className="flex h-screen justify-center items-center">
+            <div className='relative'>
+                <Image src={Concert} alt='Concert.jpg' className='brightness-75 h-screen w-[950px] object-cover' />
+                <p className='text-5xl text-white absolute top-[100px] p-2'>Enjoy the seamless ticking experience.</p>
+            </div>
+            <div className="w-[600px] h-screen flex flex-col justify-center items-center">
+                <div className='mb-6'>
+                    <Image src={DarkLogo} alt='opaqNOBG-01.png' className='h-[100px] w-[310px]'/>
+                </div>
+                    <h3 className='pb-6 text-4xl'>Sign Up</h3>
+                <form className="max-w-md mx-auto" onSubmit={handleSubmit(onSubmit)} method='post'>
+                    <div className="grid md:grid-cols-2 md:gap-6">
+                        <div className="relative z-0 w-full mb-5 group">
+                            <input type="text" { ...register('firstName') } id="floating_first_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                            { errors.firstName && (<p className='text-red-500'>{ errors.firstName.message }</p>) }
+                            <label htmlFor="floating_first_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First name</label>
+                        </div>
+                        <div className="relative z-0 w-full mb-5 group">
+                            <input type="text" { ...register('lastName') } id="floating_last_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                            { errors.lastName && (<p className='text-red-500'>{ errors.lastName.message }</p>) }
+                            <label htmlFor="floating_last_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Last name</label>
+                        </div>
+                    </div>
+                    <div className="relative z-0 w-full mb-5 group">
+                        <input type="email" { ...register('email') } id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                        { errors.email && (<p className='text-red-500'>{ errors.email.message }</p>) }
+                        <label htmlFor="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
+                    </div>
+                    <div className="relative z-0 w-full mb-5 group">
+                        <input type="text" { ...register('phone') } id="floating_phone" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                        { errors.phone && (<p className='text-red-500'>{ errors.phone.message }</p>) }
+                        <label htmlFor="floating_phone" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone number (123-456-7890)</label>
+                    </div>
+                    <div className="relative z-0 w-full mb-5 group">
+                        <input type="password" { ...register('password') } id="floating_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                        { errors.password && (<p className='text-red-500'>{ errors.password.message }</p>) }
+                        <label htmlFor="floating_password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
+                    </div>
+                    <div className="relative z-0 w-full mb-5 group">
+                        <input type="password" { ...register('confirmPassword') } id="floating_repeat_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                        { errors.confirmPassword && (<p className='text-red-500'>{ errors.confirmPassword.message }</p>) }
+                        <label htmlFor="floating_repeat_password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Confirm password</label>
+                    </div>
+                    <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 disabled:bg-blue-500" disabled={ isSubmitting }>Register</button>
+                </form>
+
+            </div>
+        </div>
+    )
+}
---
+++ src/schemas/signUpSchema.ts
@@ -0,0 +1,39 @@
+import { z } from "zod";
+
+export const signUpSchema = z.object({
+    firstName: z
+        .string()
+        .min(1, { message: "First name is required" })
+        .max(20, { message: "First name must be less than 20 characters" })
+        .regex(/^[a-zA-Z]+$/, { message: "First name must only contain letters" }),
+
+    lastName: z
+        .string()
+        .min(1, { message: "Last name is required" })
+        .max(20, { message: "Last name must be less than 20 characters" })
+        .regex(/^[a-zA-Z]+$/, { message: "Last name must only contain letters" }),
+
+    email: z
+        .string()
+        .email({ message: "Invalid email address" })
+        .refine((email) => email.endsWith('@gmail.com'), { message: "Only gmail accounts are allowed" }),
+
+    phone: z
+        .string()
+        .length(10, { message: 'Phone no. should only be of 10 numbers' }),
+
+    password: z
+        .string()
+        .min(8, { message: "Password must be at least 8 characters long" })
+        .max(20, { message: "Maximum limit of password is 20 characters" }),
+
+    confirmPassword: z.string()
+}).superRefine(({ password, confirmPassword }, ctx) => {
+    if(password !== confirmPassword) {
+        ctx.addIssue({
+            code: "custom",
+            path: ["confirmPassword"],
+            message: "Passwords do not match"
+        });
+    }
+});
